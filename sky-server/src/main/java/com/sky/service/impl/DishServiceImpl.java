c3c7e55e732604a049dbc71b90eef6ac42d3860f
ASUS <516574804@qq.com>
2026-03-19 17:57:24 +0800

    新增菜品

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,54 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    /**
+     * 新增菜品和对应口味
+     * @param dishDTO
+     */
+    @Override
+    @Transactional  //事务控制
+    public void saveWithFlavor(DishDTO dishDTO) {
+
+        //菜品表插入1条数据
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+
+        dishMapper.insert(dish);
+
+        //获取生成主键值
+        Long dishId = dish.getId();
+
+        //口味表插入n条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && !flavors.isEmpty()){
+            //为每个属性赋值
+             flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);
+             });
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
